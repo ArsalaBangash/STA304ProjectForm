dd5b79057eeb88070809449631c03ccb4438519d
ArsalaBangash <arsalabangash@gmail.com>
2016-10-24 02:06:27 -0400

    Switched to Angular

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var app1 = angular.module('app1', []);
+
+app1.controller('AppCtrl', function($scope, $http) {
+   $scope.data = {};
+
+   $scope.submitForm = function(){
+       var link = 'https://hooks.zapier.com/hooks/catch/1749362/6xyvdn/';
+
+       $http.get(link, {params: $scope.data })
+       .then(function (res){
+           $scope.response = res.data;
+       });
+   };
+});
