507dce3d4f08de9ad4cb33c507e6b10a6d56c98d
ArsalaBangash <arsalabangash@gmail.com>
2016-10-31 13:44:26 -0400

    Created a new zap die to errors in previous zap

--- js/app.js
+++ js/app.js
@@ -5,12 +5,6 @@ app1.controller('AppCtrl', function($scope, $http, $window) {
    $scope.data = {};
 
    $scope.submitForm = function(){
-       var link = "https://hooks.zapier.com/hooks/catch/1749362/6xyvdn/";
-
-       $http.get(link, {params: $scope.data })
-       .then(function (res){
-           $scope.response = res.data;
-       });
        var fullU = parseInt($scope.data.fullU, 10);
        var summU = parseInt($scope.data.summU, 10);
        var year1U = parseInt($scope.data.year1U, 10);
@@ -20,10 +14,17 @@ app1.controller('AppCtrl', function($scope, $http, $window) {
        var year5U = parseInt($scope.data.year5U, 10);
        var totalU = year1U + year2U + year3U + year4U + year5U;
        var totalTerms = fullU + summU;
+       var check = $scope.data.days;
 
        if (totalTerms != totalU) {
            $window.alert("Please revise your Upass Data");
        } else {
+           var link = 'https://hooks.zapier.com/hooks/catch/1749362/6j61v6/';
+
+           $http.get(link, {params: $scope.data })
+           .then(function (res){
+               $scope.response = res.data;
+           });
            $window.alert("Data Successfully submitted, Please reload");
            $window.location.reload();
        }
