8bb0d2b14f94d555df82fd83d62fbb9276acc947
ArsalaBangash <arsalabangash@gmail.com>
2016-10-31 11:09:57 -0400

    Re-set Zapier link in app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ app1.controller('AppCtrl', function($scope, $http, $window) {
    $scope.data = {};
 
    $scope.submitForm = function(){
-       var link = 'https://hooks.zapier.com/hooks/catch/1749362/6xyvdn/';
+       var link = "https://hooks.zapier.com/hooks/catch/1749362/6xyvdn/";
 
        $http.get(link, {params: $scope.data })
        .then(function (res){
